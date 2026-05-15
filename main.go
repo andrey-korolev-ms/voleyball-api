599342206384007a2e05ee87de7d2547e3247d44
andrey-korolev-ms <andrey-korolev@outlook.com>
2026-05-15 23:29:30 +0300

    Добавил структуры, обработки возможных ошибок вокруг json и вернул строку со значением

--- main.go
+++ main.go
@@ -11,16 +11,29 @@ type Request struct {
 	Message string `json:"message"`
 }
 
+type Response struct {
+	Message string `json:"message"`
+}
+
 func handlerAPI(w http.ResponseWriter, r *http.Request) {
 	var data Request
-	json.NewDecoder(r.Body).Decode(&data)
+	if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
+		http.Error(w, "invalid request", http.StatusBadRequest)
+		return
+	}
 
-	json.NewEncoder(w).Encode(map[string]string{"message": data.Message})
+	if data.Message == "" {
+		http.Error(w, "message is required", http.StatusBadRequest)
+		return
+	}
+	res := Response{Message: fmt.Sprintf("Response: %s", data.Message)}
+
+	json.NewEncoder(w).Encode(res)
 }
 
 func main() {
 
 	http.HandleFunc("/api", handlerAPI)
-	http.ListenAndServe(":8080", nil)
 	fmt.Println("Server started on port :8080")
+	http.ListenAndServe(":8080", nil)
 }
