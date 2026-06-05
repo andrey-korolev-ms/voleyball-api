aaea889e15f772c90bafc5ef9d88931eeaf545c3
andrey-korolev-ms <andrey-korolev@outlook.com>
2026-06-05 18:04:44 +0300

    Удалил предыдущие изменения!

--- go.mod
+++
@@ -1,5 +0,0 @@
-module voleyball-api
-
-go 1.26.2
-
-require github.com/lib/pq v1.12.3 // indirect
--- main.go
+++ main.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"database/sql"
 	"encoding/json"
 	"fmt"
 
 	"net/http"
-
-	_ "github.com/lib/pq"
 )
 
 type Request struct {
@@ -18,16 +15,10 @@ type Response struct {
 	Message string `json:"message"`
 }
 
-type Player struct {
-	ID    int
-	Name  string
-	Email string
-}
-
 func handlerAPI(w http.ResponseWriter, r *http.Request) {
 	var data Request
 	if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
-		http.Error(w, "invalid2 request", http.StatusBadRequest)
+		http.Error(w, "invalid request", http.StatusBadRequest)
 		return
 	}
 
@@ -35,47 +26,9 @@ func handlerAPI(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "message is required", http.StatusBadRequest)
 		return
 	}
+	res := Response{Message: fmt.Sprintf("Response: %s", data.Message)}
 
-	dsn := "host=localhost port=5432 user=postgres password=111 dbname=test1 sslmode=disable"
-
-	// Открываем подключение
-	db, err := sql.Open("postgres", dsn)
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
-
-	// Проверка подключения
-	err = db.Ping()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("✅ Подключено к PostgreSQL!")
-
-	// Читаем данные
-	rows, err := db.Query("SELECT * FROM users")
-	if err != nil {
-		panic(err)
-	}
-	defer rows.Close()
-
-	// Обработка результатов
-	var players []Player = make([]Player, 0)
-	for rows.Next() {
-		var p Player
-		if err := rows.Scan(&p.ID, &p.Name, &p.Email); err != nil {
-			http.Error(w, fmt.Sprintf("scan error: %v", err), http.StatusInternalServerError)
-			return
-		}
-		fmt.Printf("ID: %d, Имя: %s, Email: %s\n", p.ID, p.Name, p.Email)
-		players = append(players, p)
-	}
-	if len(players) == 0 {
-		http.Error(w, "no players found", http.StatusNotFound)
-		return
-	}
-
-	json.NewEncoder(w).Encode(players)
+	json.NewEncoder(w).Encode(res)
 }
 
 func main() {
